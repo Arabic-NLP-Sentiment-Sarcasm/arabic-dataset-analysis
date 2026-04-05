10af9db11565049de11766e4774cedf2493131a8
ali221217 <s2212172438@ku.edu.kw>
2026-04-05 19:24:03 +0300

    Updated

--- data/script_data.py
+++ data/script_data.py
@@ -1,1 +1,28 @@
+#Source of Data and data descreption
+import pandas as pd
 
+print("\nDatasets: ArSarcasm & Arabic Sentiment Tweets")
+
+#ArSarcasm Dataset
+df = pd.read_csv("./ArSarcasm_sample.csv")
+print("\nArSarcasm Dataset")
+print("Task: Arabic sarcasm detection")
+print("Source: https://github.com/iabufarha/ArSarcasm")
+
+print("===Sample of Data====")
+print(df.head(5))
+print("__________________________")
+#Arabic Sentiment Tweets Dataset
+df = pd.read_csv(
+    "./Sentiment_tweets_sample.txt",
+    sep="\t",                
+    names=["sentence", "sentiment"], 
+    encoding="utf-8"
+)
+print("\nASTD: Arabic Sentiment Tweets Dataset")
+
+print("Task: Arabic Sentiment Tweets")
+print("Source: https://github.com/mahmoudnabil/ASTD")
+
+print("=== Sample of Data ===")
+print(df.head(5))
